68d7714a783706f083e2f5debd5a0bb4f671f17d
nobindebnath88888-hue <nobindebnath88888@gmail.com>
2026-03-09 12:10:18 +0600

    Update playerManager.js

--- core/playerManager.js
+++ core/playerManager.js
@@ -1,1 +1,23 @@
+// core/playerManager.js
 
+class PlayerManager {
+  constructor() {
+    this.players = [];
+  }
+
+  addPlayer(name, color) {
+    const player = { name, color, alive: true };
+    this.players.push(player);
+    return player;
+  }
+
+  removePlayer(color) {
+    this.players = this.players.filter(p => p.color !== color);
+  }
+
+  getAlivePlayers() {
+    return this.players.filter(p => p.alive);
+  }
+}
+
+module.exports = PlayerManager;
