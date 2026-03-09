9fe2d926510e36ed47f654a99bd9bf89fe002ab0
nobindebnath88888-hue <nobindebnath88888@gmail.com>
2026-03-09 12:10:44 +0600

    Update roomManager.js

--- core/roomManager.js
+++ core/roomManager.js
@@ -1,1 +1,28 @@
+// core/roomManager.js
 
+class RoomManager {
+  constructor() {
+    this.rooms = {}; // {roomID: {engine: ChaturajiEngine, players: []}}
+  }
+
+  createRoom(roomID) {
+    this.rooms[roomID] = { engine: null, players: [] };
+  }
+
+  joinRoom(roomID, player) {
+    if (!this.rooms[roomID]) throw "Room does not exist";
+    this.rooms[roomID].players.push(player);
+  }
+
+  setEngine(roomID, engine) {
+    if (!this.rooms[roomID]) throw "Room does not exist";
+    this.rooms[roomID].engine = engine;
+  }
+
+  getRoomState(roomID) {
+    if (!this.rooms[roomID]) throw "Room does not exist";
+    return this.rooms[roomID].engine.getGameState();
+  }
+}
+
+module.exports = RoomManager;
